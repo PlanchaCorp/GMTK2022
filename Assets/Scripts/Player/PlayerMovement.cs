cd279bbe26fba5bd5bae4cb5c380a0b3280c3b03
Olivier Brissez <olivier@brissez.com>
2022-07-15 23:29:56 +0200

    feat: implement new input system

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private UnityAtoms.AtomBaseVariable<Vector2> playerMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
