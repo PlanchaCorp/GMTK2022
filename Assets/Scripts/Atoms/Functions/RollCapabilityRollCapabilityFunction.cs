8410f3cf8829ae56f95648b67ffe76948e78a9b6
Olivier Brissez <olivier@brissez.com>
2022-07-22 00:56:48 +0200

    refactor: prepare all scene scriptable objects

--- Assets/Scripts/Atoms/Functions/RollCapabilityRollCapabilityFunction.cs
+++
@@ -1,8 +0,0 @@
-namespace UnityAtoms
-{
-    /// <summary>
-    /// Function x 2 of type `RollCapability`. Inherits from `AtomFunction&lt;RollCapability, RollCapability&gt;`.
-    /// </summary>
-    [EditorIcon("atom-icon-sand")]
-    public abstract class RollCapabilityRollCapabilityFunction : AtomFunction<RollCapability, RollCapability> { }
-}
